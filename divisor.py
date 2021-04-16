14d66262ffbfff95a1b26b850c276e56ef574f0a
Linshengyi-William <sunlins@oregonstate.edu>
2021-04-15 17:07:36 -0700

    divisor function done

--- divisor.py
+++ divisor.py
@@ -1,5 +1,7 @@
 def divisor(a):
     for i in range(1, a + 1):
-        pass
+        if a % i == 0:
+            print(i)
 
-inp = int(input("Enter desired number to find divisor"))
+inp = int(input("Enter desired number to find divisor: "))
+divisor(inp)

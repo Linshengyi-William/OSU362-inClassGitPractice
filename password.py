c31b8a9430e1a2c698268e0ab9af02ab26ed0acd
Linshengyi-William <sunlins@oregonstate.edu>
2021-04-15 17:17:06 -0700

    prompt user to input

---
+++ password.py
@@ -0,0 +1,13 @@
+import random
+import string
+
+def password(a):
+    pass
+    #l = string.ascii_letters
+    #result = ''.join(random.choice(l) for i in range(a))
+    #return result
+
+
+inp = int(input("Enter desired password length: "))
+
+#print("password is: ", password(inp))

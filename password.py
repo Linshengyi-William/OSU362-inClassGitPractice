61c7011c500c9c0aedc935e5a915b267694cc305
Linshengyi-William <sunlins@oregonstate.edu>
2021-04-15 17:17:42 -0700

    finished password function

--- password.py
+++ password.py
@@ -3,11 +3,11 @@ import string
 
 def password(a):
     pass
-    #l = string.ascii_letters
-    #result = ''.join(random.choice(l) for i in range(a))
-    #return result
+    l = string.ascii_letters
+    result = ''.join(random.choice(l) for i in range(a))
+    return result
 
 
 inp = int(input("Enter desired password length: "))
 
-#print("password is: ", password(inp))
+print("password is: ", password(inp))
